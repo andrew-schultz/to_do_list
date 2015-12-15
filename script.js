70c5a8829e21c63c42707f5c354e660ae48c5924
Andrew Schultz <as173171@gmail.com>
2015-12-14 23:01:15 -0500

    changed up the style, added a timestamp feature to the new posts

--- script.js
+++ script.js
@@ -3,44 +3,41 @@ $(document).ready(function(){
 	$('#newInputForm').on('submit', function(e){
 	//when the submit button on the form is clicked, do this
 		e.preventDefault();
+		var now = new Date();
+		var year = now.getFullYear();
+		var month = now.getMonth() + 1;
+		var day = now.getDate();
+		var timestamp = month + "/" + day + "/" + year
 		//prevents the default function of the submit button from firing
 		//doesn't send the input away
-		//console.log($("#newInput").val());
-		if ($('#newInput').val().length > 1){
-				$('ul').append('<li><p class="text">' + $("#newInput").val() + '</p><i class="fa fa-check-square"></i></li>');
+		if ($('#newInput').val().length >= 1){
+				$('ul' ).append('<li><i class="fa fa-times-circle"></i><p>' + $("#newInput").val() + ' <span>' + timestamp + '</span></p></li>');
 			//this adds an item to the list with the same formatting as the ones that are already there
 			};
 		$('#newInput').val("");
 		//this just clears the previous input from the text box
-		$('li').on('click', function(){
-			$(this).toggleClass('complete');
-		});
 
 		$('.fa').on('click', function(){
-			$(this).parent().remove();
-		});
-		
+		//selects the class .fa on click, runs a function
+			$(this).parent().fadeOut(200 ,function(){
+			//fades out the element with the .fa class
+				$(this).remove();
+				//removes the element completely from the html
+			});
+		});	
 	});
-
-	$('li').on('click', function(){
+	$('ul').on('click', 'li', function(){
+	//selects the ul element on click, runs a function
 		$(this).toggleClass('complete');
-	});
+	}); //toggles the class complete
 
 	$('.fa').on('click', function(){
-		$(this).parent().fadeOut(500 ,function(){
+	//selects the class .fa on click, runs a function
+		$(this).parent().fadeOut(200 ,function(){
+		//fades out the element with the .fa class
 			$(this).remove();
-
+			//removes the element completely from the html
 		});
-		$(this).parent().hasClass('complete');
 	});
-
-	// $('.fa').on('click', function(){
-	// 	if ($('li').hasClass('complete')){
-	// 		console.log('got it');
-	// 		$(this).remove();
-	// 	} else {
-	// 		console.log('nope');
-	// 	};
-	// });
 });
 
